37a8fb0345fcb678f191494509acaf7cf2ab5f83
Epicsociety <measkswho@gmail.com>
2023-01-20 01:13:24 +0300

    Push and pall

--- stack_functions.c
+++ stack_functions.c
@@ -9,12 +9,12 @@
  */
 void push(stack_t **stack, unsigned int line_number, char *n)
 {
-	stack_t *new = NULL;
+	stack_t *new_node = NULL;
 	int i;
 
 	if (n == NULL || (n[0] == '-' && !isdigit(n[0])))
 	{
-		printf("L%d: usage: push integer\n", line_number);
+		fprintf(stderr, "L%d: usage: push integer\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 
@@ -22,28 +22,28 @@ void push(stack_t **stack, unsigned int line_number, char *n)
 	{
 		if (isdigit(n[i]) == 0)
 		{
-			printf("L%d: usage: push integer\n", line_number);
+			fprintf(stderr, "L%d: usage: push integer\n", line_number);
 			exit(EXIT_FAILURE);
 		}
 	}
 
-	new = malloc(sizeof(stack_t));
-	if (new == NULL)
+	new_node = malloc(sizeof(stack_t));
+	if (new_node == NULL)
 	{
-		printf("Error: malloc failed\n");
+		fprintf(stderr, "Error: malloc failed\n");
 		exit(EXIT_FAILURE);
 	}
 
-	new->n = atoi(n);
-	new->prev = NULL;
-	new->next = NULL;
+	new_node->n = atoi(n);
+	new_node->prev = NULL;
+	new_node->next = NULL;
 	if (*stack != NULL)
 	{
-		new->next = *stack;
-		(*stack)->prev = new;
+		new_node->next = *stack;
+		(*stack)->prev = new_node;
 	}
 
-	*stack = new;
+	*stack = new_node;
 }
 
 /**
